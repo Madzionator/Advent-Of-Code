7a586351b173886b77f69eb6621c4da4ec13d07d
Madziks <majta.magdalena@gmail.com>
2020-12-05 14:42:19 +0100

    Day 5*

--- Week1/Day5.cs
+++ Week1/Day5.cs
@@ -27,20 +27,16 @@ namespace Advent._2020.Week1
 
         private static int Task_A(string line)
         {
-            string row = line.Substring(0, 7).Replace('F', '0').Replace('B', '1');
-            string column = line.Substring(7, line.Length - 7).Replace('L', '0').Replace('R', '1');
-            int id = Convert.ToInt32(row, 2) * 8 + Convert.ToInt32(column, 2);
-            return id;
+            string binary_number = line.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
+            return Convert.ToInt32(binary_number, 2);
         }
 
         private static int Task_B(List<int>results)
         {
             var ordered_results = results.OrderBy(x => x).ToList();
             for(int i = ordered_results[1]; i<ordered_results.Count; i++)
-            {
                 if (ordered_results[i] - ordered_results[i - 1] > 1)
                     return ordered_results[i] - 1;
-            }
             return -1;
         }
     }
