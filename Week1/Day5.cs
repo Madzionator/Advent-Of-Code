c367faeaab8fefe0b47af45ddeda2e1413394ed9
Madziks <majta.magdalena@gmail.com>
2020-12-05 19:12:36 +0100

    Day 5

--- Week1/Day5.cs
+++ Week1/Day5.cs
@@ -9,19 +9,19 @@ namespace Advent._2020.Week1
     {
         public static void Execute()
         {
-            List<int> results = new List<int>();
+            List<int> data = new List<int>();
             var lines = File.ReadAllLines(@"Week1\input5.txt");
             foreach (var line in lines)
-                results.Add(Task_A(line));
+                data.Add(Convert_to_binary(line));
 
-            int result_A = results.Max();
-            int result_B = Task_B(results);
+            int result_A = data.Max();
+            int result_B = Task_B(data);
 
             Console.WriteLine(result_A);
             Console.WriteLine(result_B);
         }
 
-        private static int Task_A(string line)
+        private static int Convert_to_binary(string line)
         {
             string binary_number = line.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
             return Convert.ToInt32(binary_number, 2);
