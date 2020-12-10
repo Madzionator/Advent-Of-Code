81e658651ce75d0806b9b25f224a8b1775158058
Madziks <majta.magdalena@gmail.com>
2020-12-10 20:52:11 +0100

    Day 10

--- Week2/Day10.cs
+++ Week2/Day10.cs
@@ -9,51 +9,56 @@ namespace Advent._2020.Week2
     {
         public static void Execute()
         {
-            var joltageRatings = File.ReadAllLines(@"Week2\input10.txt").Select(int.Parse).OrderBy(x => x).ToList();
-            joltageRatings.Insert(0, 0);
-            joltageRatings.Add(joltageRatings[^1] + 3);
+            var numbers = File.ReadAllLines(@"Week2\input10.txt")
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .ToList();
 
-            int resultA = TaskA(joltageRatings);
-            long resultB = TaskB(joltageRatings);
+            numbers.Insert(0, 0);
+            numbers.Add(numbers[^1] + 3);
+
+            int resultA = TaskA(numbers);
+            long resultB = TaskB(numbers);
 
             Console.WriteLine(resultA);
             Console.WriteLine(resultB);
         }
 
-        private static int TaskA(List<int> joltageRatings)
+        private static int TaskA(List<int> numbers)
         {
             int diff1 = 0;
             int r3 = 0;
-            for (int i = 0; i < joltageRatings.Count - 1; i++)
-                if (joltageRatings[i + 1] - joltageRatings[i] == 1)
+            for (int i = 0; i < numbers.Count - 1; i++)
+                if (numbers[i + 1] - numbers[i] == 1)
                     diff1++;
-                else if (joltageRatings[i + 1] - joltageRatings[i] == 3)
+                else if (numbers[i + 1] - numbers[i] == 3)
                     r3++;
 
             return diff1 * r3;
         }
 
-        private static long TaskB(List<int> joltageRatings)
+        private static long TaskB(List<int> numbers)
         {
-            List <long> previousSum = new List<long> { 1, 1, 1 };
-            for (int i = joltageRatings.Count - 2; i > 0; i--)
+            long[] previousSum = new long[] { 1, 1, 1 };
+            for (int i = numbers.Count - 2; i > 0; i--)
             {
                 long currentSum = 0;
 
-                for(int j = 1; j<=3; j++)
-                    if (i + j < joltageRatings.Count && joltageRatings[i + j] - joltageRatings[i] <= 3)
-                        currentSum += previousSum[j-1];
+                for (int j = 1; j <= 3; j++)
+                    if (i + j < numbers.Count && numbers[i + j] - numbers[i] <= 3)
+                        currentSum += previousSum[j - 1];
 
-                previousSum.Insert(0, currentSum);
-                previousSum.RemoveAt(3);
+                previousSum[2] = previousSum[1];
+                previousSum[1] = previousSum[0];
+                previousSum[0] = currentSum;
             }
 
             long total = 0;
-            for(int i = 1; i<=3; i++)
-                if (joltageRatings.Contains(i))
+            for (int i = 1; i <= 3; i++)
+                if (numbers.Contains(i))
                     total += previousSum[i - 1];
 
             return total;
         }
     }
-}
+} 
