8a25bd76925314b22e7fb8d2591825c780cb5f78
Madziks <majta.magdalena@gmail.com>
2020-12-31 03:00:59 +0100

    AoC2020 ⭐⭐💯🎄

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Advent._2020
             var s = new Stopwatch();
             s.Start();
 
-            Day19.Execute();
+            Day25.Execute();
 
             s.Stop();
             Console.WriteLine($"\n\nTime Elapsed: {s.ElapsedMilliseconds}ms");
