0cbbb93f79e54920da51501a1258a233ded0621a
Madziks <majta.magdalena@gmail.com>
2021-11-30 16:36:44 +0100

    refactor

---
+++ AOC_2021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Advent._2021.Week1;
+
+namespace Advent._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Stopwatch();
+            s.Start();
+
+            Day1.Execute();
+
+            s.Stop();
+            Console.WriteLine($"\n\nTime Elapsed: {s.ElapsedMilliseconds}ms");
+        }
+    }
+}
---
+++ AOC_2021/Week1/Day1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Advent._2021.Week1
+{
+    class Day1
+    {
+        public static void Execute()
+        {
+            var file = File.ReadAllLines(@"Week1\input1.txt");
+            var report = file.Select(int.Parse).ToList();
+
+            Console.WriteLine(Task_A(report));
+            Console.WriteLine(Task_B(report));
+        }
+
+        public static int Task_A(List<int> report)
+        {
+            return 0;
+        }
+
+        public static int Task_B(List<int> report)
+        {
+            return 0;
+        }
+    }
+}
