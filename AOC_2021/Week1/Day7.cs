3cd0ce79d7ce8efd4bd92b2226c276e321cf31db
Magdalena Majta <majta.magdalena@gmail.com>
2021-12-07 14:31:05 +0100

    Day 7 ⭐⭐

---
+++ AOC_2021/Week1/Day7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent._2021.Week1
+{
+    class Day7
+    {
+        public static void Execute()
+        {
+            var positions = File.ReadAllText(@"Week1\input7.txt").Split(',').Select(int.Parse).ToArray();
+
+            Console.WriteLine(Task(positions, x => x));                 // TaskA
+            Console.WriteLine(Task(positions, x => x * (1 + x) / 2));   // TaskB
+        }
+
+        public static int Task(int[] positions, Func<int, int> operation) => 
+            Enumerable.Range(positions.Min(), positions.Max() + 1)
+                .Select(x => positions
+                    .Select(position => Math.Abs(position - x))
+                    .Select(move => operation(move))
+                    .Sum()).Min();
+    }
+}
