0ea5442a470d1140d46f8ef76d9e0aac996c7472
Magdalena Majta <majta.magdalena@gmail.com>
2022-12-04 08:51:59 +0100

    Day 4 ⭐⭐

---
+++ AOC_2022/Week1/Day4.cs
@@ -0,0 +1,24 @@
+﻿using Advent._2022.Day;
+
+namespace Advent._2022.Week1;
+
+class Day4 : IDay
+{
+    public void Execute()
+    {
+        var input = File.ReadAllLines(@"Week1\input4.txt")
+            .Select(x => x
+                .Split(',', '-')
+                .Select(int.Parse)
+                .ToArray());
+
+        Console.WriteLine(TaskA(input));
+        Console.WriteLine(TaskB(input));
+    }
+
+    private int TaskA(IEnumerable<int[]> sections) => 
+        sections.Count(s => (s[0] >= s[2] && s[1] <= s[3]) || (s[0] <= s[2] && s[1] >= s[3]));
+
+    private int TaskB(IEnumerable<int[]> sections) => 
+        sections.Count(s => (s[0] <= s[2] && s[1] >= s[2]) || (s[0] > s[2] && s[0] <= s[3]));
+}
