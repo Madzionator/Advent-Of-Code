a01a0041a6f711a81465495bcb5f44503ba6892b
Magdalena Majta <majta.magdalena@gmail.com>
2022-12-05 15:43:11 +0100

    Day 5 ⭐⭐

---
+++ AOC_2022/Week1/Day5.cs
@@ -0,0 +1,51 @@
+﻿using Advent._2022.Day;
+
+namespace Advent._2022.Week1;
+
+class Day5 : IDay
+{
+    public void Execute()
+    {
+        var input = File.ReadAllText(@"Week1\input5.txt")
+            .Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Split(Environment.NewLine))
+            .ToArray();
+
+        var procedure = input[1]
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => (int.Parse(x[1]), int.Parse(x[3]) - 1, int.Parse(x[5]) - 1));
+
+        var stacks = new string[(input[0][0].Length+1)/4];
+        for (var i = input[0].Length - 2; i >= 0; i--)
+        for (var j = 1; j < input[0][0].Length; j += 4)
+                stacks[(j - 1) / 4] += input[0][i][j] == ' ' ? "" : input[0][i][j];
+
+        Console.WriteLine(TaskA(stacks.ToArray(), procedure));
+        Console.WriteLine(TaskB(stacks, procedure));
+    }
+
+    private string TaskA(string[] stacks, IEnumerable<(int, int, int)> procedure)
+    {
+        foreach (var (quantity, from, to) in procedure)
+            for (var i = 0; i < quantity; i++)
+            {
+                var toMove = stacks[from][^1];
+                stacks[from] = stacks[from][..^1];
+                stacks[to] += toMove;
+            }
+
+        return string.Join("", stacks.Select(x => x[^1]));
+    }
+
+    private string TaskB(string[] stacks, IEnumerable<(int, int, int)> procedure)
+    {
+        foreach (var (quantity, from, to) in procedure)
+        {
+            var toMove = stacks[from][(stacks[from].Length - quantity)..stacks[from].Length];
+            stacks[from] = stacks[from][..(stacks[from].Length - quantity)];
+            stacks[to] += toMove;
+        }
+
+        return string.Join("", stacks.Select(x => x[^1]));
+    }
+}
--- AdventHelpers/Extensions/MatrixUtils.cs
+++ AdventHelpers/Extensions/MatrixUtils.cs
@@ -4,7 +4,50 @@ namespace Advent.Helpers.Extensions
 {
     public static class MatrixUtils
     {
-        public static int[,] ToMatrix (this string[] array)
+        public static T[,] ToMatrix<T>(this T[][] array)
+        {
+            var matrix = new T[array.Length, array[0].Length];
+
+            for (var y = 0; y < array.Length; y++)
+            for (var x = 0; x < array[0].Length; x++)
+                matrix[y, x] = array[y][x];
+
+            return matrix;
+        }
+
+        public static T[,] Transpose<T>(this T[,] matrix)
+        {
+            var m = matrix.GetLength(0);
+            var n = matrix.GetLength(1);
+
+            var newMatrix = new T[n, m];
+            for (var y = 0; y < n; y++)
+            for (var x = 0; x < m; x++)
+                newMatrix[y, x] = matrix[x, y];
+
+            return newMatrix;
+        }
+
+        public static void OnEachInMatrix<T>(this T[,] matrix, Func<T, T> operation)
+        {
+            for(var y = 0; y < matrix.GetLength(0); y++)
+            for (var x = 0; x < matrix.GetLength(1); x++)
+                matrix[y, x] = operation(matrix[y, x]);
+        }
+
+        public static void DrawMatrix<T>(this T[,] matrix, string separator = "")
+        {
+            for (var y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (var x = 0; x < matrix.GetLength(1); x++)
+                    Console.Write(matrix[y, x].ToString() + separator);
+                Console.WriteLine();
+            }
+        }
+
+        public static T[,] CopyMatrix<T>(this T[,] matrix) => matrix.Clone() as T[,];
+
+        public static int[,] ToMatrix(this string[] array)
         {
             var matrix = new int[array.Length, array[0].Length];
 
@@ -27,24 +70,5 @@ namespace Advent.Helpers.Extensions
 
             return matrix;
         }
-
-        public static void OnEachInMatrix<T>(this T[,] matrix, Func<T, T> operation)
-        {
-            for(var y = 0; y < matrix.GetLength(0); y++)
-            for (var x = 0; x < matrix.GetLength(1); x++)
-                matrix[y, x] = operation(matrix[y, x]);
-        }
-
-        public static void DrawMatrix<T>(this T[,] matrix, string separator = "")
-        {
-            for (var y = 0; y < matrix.GetLength(0); y++)
-            {
-                for (var x = 0; x < matrix.GetLength(1); x++)
-                    Console.Write(matrix[y, x].ToString() + separator);
-                Console.WriteLine();
-            }
-        }
-
-        public static T[,] CopyMatrix<T>(this T[,] matrix) => matrix.Clone() as T[,];
     }
 }
