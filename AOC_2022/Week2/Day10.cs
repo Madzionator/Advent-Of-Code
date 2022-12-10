0438867838c263e495ed9f72a5c27cfdff54a62a
Magdalena Majta <majta.magdalena@gmail.com>
2022-12-10 11:36:36 +0100

    Day 10 ⭐⭐

--- AOC_2022/Week2/Day10.cs
+++ AOC_2022/Week2/Day10.cs
@@ -6,19 +6,50 @@ class Day10 : IDay
 {
     public void Execute()
     {
-        var input = File.ReadAllLines(@"Week2\input10.txt");
+        var input = File.ReadAllLines(@"Week2\input10.txt")
+            .Select(x => x.Split(' '))
+            .Select(x => x[0] == "noop" ? (1, 0) : (2, int.Parse(x[1])))
+            .ToList();
 
-        Console.WriteLine(TaskA(input));
-        Console.WriteLine(TaskB(input));
+        Console.WriteLine(Task(input));
     }
 
-    private int TaskA(string[] input)
+    private int Task(List<(int Cl, int Val)> input)
     {
-        return 0;
-    }
-    
-    private int TaskB(string[] input)
-    {
-        return 0;
+        int X = 1;
+        var instr = (-1, 0, 0); //instr nr, step, toAdd
+        int result = 0;
+
+        for (var clock = 1;; clock++)
+        {
+            if (instr.Item2 == 0)
+            {
+                instr.Item1++;
+                if (instr.Item1 >= input.Count)
+                    break;
+
+                instr.Item2 = input[instr.Item1].Cl;
+                instr.Item3 = input[instr.Item1].Val;
+            }
+
+            if(clock%40-2 <= X && X <= clock%40)
+                Console.Write('#');
+            else
+                Console.Write(' ');
+
+            if (clock%40 == 0)
+                Console.Write(Environment.NewLine);
+
+            if ((clock - 20) % 40 == 0)
+                result += clock * X;
+
+            if (instr.Item2 == 1)
+                X += instr.Item3;
+
+            instr.Item2--;
+        }
+
+        Console.Write(Environment.NewLine);
+        return result;
     }
 }
---
+++ AdventHelpers/Extensions/BasicUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Advent.Helpers.Extensions
+{
+    public static class BasicUtils
+    {
+        public static bool IsBetween(this int X, int A, int B)
+        {
+            var min = Math.Min(A, B);
+            var max = Math.Max(A, B);
+
+            return min <= X && X <= max;
+        }
+    }
+}
