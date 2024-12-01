b44e800770955fb96f6191712fc5c3e9fffe2426
Magdalena Majta <majta.magdalena@gmail.com>
2024-12-01 09:16:47 +0100

    Day 1 - 2024 ⭐⭐

--- AOC_2024/Week1/Day1.cs
+++ AOC_2024/Week1/Day1.cs
@@ -6,19 +6,28 @@ class Day1 : IDay
     {
         var input = File.ReadAllText(@"Week1\input1.txt")
             .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Split("  ").Select(int.Parse).ToArray())
+            .Select(x => (L: x[0], R: x[1]))
             .ToList();
 
         Console.WriteLine($"A: {TaskA(input)}");
         Console.WriteLine($"B: {TaskB(input)}");
     }
 
-    int TaskA(List<string> input)
+    long TaskA(List<(int L, int R)> input)
     {
-        return 0;
+        return input.Select(x => x.L).Order()
+            .Zip(input.Select(x => x.R).Order(), 
+                (l, r) => Math.Abs(l - r))
+            .Sum();
     }
 
-    int TaskB(List<string> input)
+    int TaskB(List<(int L, int R)> input)
     {
-        return 0;
+        var leftCount = input.Select(x => x.L).CountBy(x => x).ToDictionary();
+        var rightCount = input.Select(x => x.R).CountBy(x => x).ToDictionary();
+
+        return leftCount.Aggregate(0, (sum, left) 
+            => sum + left.Key * left.Value * rightCount.GetValueOrDefault(left.Key, 0));
     }
 }
