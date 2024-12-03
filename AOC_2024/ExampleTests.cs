c469c58170b933e475504c8dce153383378b5c6f
Magdalena Majta <majta.magdalena@gmail.com>
2024-12-03 14:30:51 +0100

    Day 3 ⭐⭐💻

--- AOC_2024/ExampleTests.cs
+++ AOC_2024/ExampleTests.cs
@@ -10,6 +10,10 @@ public class ExampleTests
     public void Day1() => ExecuteDay<Day1>("Week1/example1_1.txt", "11", "31");
     [Fact]
     public void Day2() => ExecuteDay<Day2>("Week1/example2_1.txt", "2", "4");
+    [Theory]
+    [InlineData(1, "161", "")]
+    [InlineData(2, "", "48")]
+    public void Day3(int ex, string a, string b) => ExecuteDay<Day3>($"Week1/example3_{ex}.txt", a, b);
 
     private static void ExecuteDay<TDay>(string path, string expectedResultA, string expectedResultB) where TDay : Day
     {
---
+++ AOC_2024/Week1/Day3.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Week1;
+
+class Day3 : Day
+{
+    private const string MulPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+    public override (object resultA, object resultB) Execute()
+    {
+        var input = string.Join("", InputLines);
+        return (TaskA(input), TaskB(input));
+    }
+
+    int TaskA(string input) 
+        => Regex.Matches(input, MulPattern)
+            .Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value))
+            .Sum();
+
+    int TaskB(string input) 
+        => input.Split("do")
+            .Where(x => !x.StartsWith("n't()"))
+            .Select(TaskA)
+            .Sum();
+}
