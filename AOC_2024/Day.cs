8f9c3a6c9f085cefd685db0331dbaa7eeb484aa0
Magdalena Majta <majta.magdalena@gmail.com>
2024-12-10 21:30:44 +0100

    Time Measurement ⏳

--- AOC_2024/Day.cs
+++ AOC_2024/Day.cs
@@ -19,14 +19,11 @@ public abstract class Day
             throw new Exception("Input file is empty");
         }
 
-        var s = Stopwatch.StartNew();
-        var results = Execute();
-        s.Stop();
-        
-        Console.WriteLine($"A: {results.resultA}");
-        Console.WriteLine($"B: {results.resultB}");
-
-        Console.WriteLine($"\n\nTime Elapsed: {s.Elapsed.Humanize(precision: 2)}");
+#if DEBUG
+        BasicTimeMeasurement();
+#else
+        TimeMeasurement();
+#endif
     }
 
     public abstract (object resultA, object resultB) Execute();
@@ -36,5 +33,42 @@ public abstract class Day
         var week = (day - 1) / 7 + 1;
         var fileName = isExample ? $"example{day}.txt" : $"input{day}.txt";
         return Path.Combine($"Week{week}", fileName);
+    }
+
+    private void BasicTimeMeasurement()
+    {
+        var s = Stopwatch.StartNew();
+        var results = Execute();
+        s.Stop();
+
+        Console.WriteLine($"A: {results.resultA}");
+        Console.WriteLine($"B: {results.resultB}");
+
+        Console.WriteLine($"\n\nTime Elapsed: {s.Elapsed.Humanize(precision: 2)}");
+    }
+
+    private void TimeMeasurement()
+    {
+        // warm up
+        var results = Execute();
+        for (int i = 0; i < 4; i++) 
+        {
+            Execute();
+        }
+
+        // measurement
+        var repeats = 10;
+        var s = Stopwatch.StartNew();
+        for (int i = 0; i < repeats; i++)
+        {
+            Execute();
+        }
+        s.Stop();
+
+        Console.WriteLine($"A: {results.resultA}");
+        Console.WriteLine($"B: {results.resultB}");
+
+        var mean = s.Elapsed / repeats;
+        Console.WriteLine($"\n\nMean elapsed time: {mean.Humanize(precision: 4)}");
     }
 }
