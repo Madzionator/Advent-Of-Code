0c3c9141a137c67735097a806288a804a8eacc56
Magdalena Majta <majta.magdalena@gmail.com>
2024-12-10 11:20:35 +0100

    Day 10 ⭐⭐🌋

--- AOC_2024/ExampleTests.cs
+++ AOC_2024/ExampleTests.cs
@@ -10,27 +10,27 @@ public class ExampleTests
     #region Week1
 
     [Fact]
-    public void Day1() => ExecuteDay<Day1>("Week1/example1_1.txt", "11", "31");
+    public void Day01() => ExecuteDay<Day1>("Week1/example1_1.txt", "11", "31");
 
     [Fact]
-    public void Day2() => ExecuteDay<Day2>("Week1/example2_1.txt", "2", "4");
+    public void Day02() => ExecuteDay<Day2>("Week1/example2_1.txt", "2", "4");
 
     [Theory]
     [InlineData(1, "161", "")]
     [InlineData(2, "", "48")]
-    public void Day3(int ex, string a, string b) => ExecuteDay<Day3>($"Week1/example3_{ex}.txt", a, b);
+    public void Day03(int ex, string a, string b) => ExecuteDay<Day3>($"Week1/example3_{ex}.txt", a, b);
 
     [Fact]
-    public void Day4() => ExecuteDay<Day4>("Week1/example4_1.txt", "18", "9");
+    public void Day04() => ExecuteDay<Day4>("Week1/example4_1.txt", "18", "9");
 
     [Fact]
-    public void Day5() => ExecuteDay<Day5>("Week1/example5_1.txt", "143", "123");
+    public void Day05() => ExecuteDay<Day5>("Week1/example5_1.txt", "143", "123");
 
     [Fact]
-    public void Day6() => ExecuteDay<Day6>("Week1/example6_1.txt", "41", "6");
+    public void Day06() => ExecuteDay<Day6>("Week1/example6_1.txt", "41", "6");
 
     [Fact]
-    public void Day7() => ExecuteDay<Day7>("Week1/example7_1.txt", "3749", "11387");
+    public void Day07() => ExecuteDay<Day7>("Week1/example7_1.txt", "3749", "11387");
 
     #endregion
 
@@ -39,10 +39,13 @@ public class ExampleTests
     [Theory]
     [InlineData(1, "14", "34")]
     [InlineData(2, "", "9")]
-    public void Day8(int ex, string a, string b) => ExecuteDay<Day8>($"Week2/example8_{ex}.txt", a, b);
+    public void Day08(int ex, string a, string b) => ExecuteDay<Day8>($"Week2/example8_{ex}.txt", a, b);
 
     [Fact]
-    public void Day9() => ExecuteDay<Day9>("Week2/example9_1.txt", "1928", "2858");
+    public void Day09() => ExecuteDay<Day9>("Week2/example9_1.txt", "1928", "2858");
+
+    [Fact]
+    public void Day10() => ExecuteDay<Day10>("Week2/example10_1.txt", "36", "81");
 
     #endregion
 
---
+++ AOC_2024/Week2/Day10.cs
@@ -0,0 +1,71 @@
+﻿using AdventOfCode2024.Helpers;
+
+namespace AdventOfCode2024.Week2;
+
+internal class Day10 : Day
+{
+    private int[,] _map;
+    private Vector2[] _9positions;
+    private Direction2[] _directions;
+
+    public override (object resultA, object resultB) Execute()
+    {
+        _map = InputLines.ToMatrix();
+        _9positions = InputLines.SelectMany((line, y) => line
+                .Select((i, x) => new { y, x, c = i })
+                .Where(v => v.c == '9')
+                .Select(v => new Vector2(v.y, v.x)))
+            .ToArray();
+
+        _directions = Direction2.Sides;
+
+        return Task();
+    }
+
+    (int, int) Task()
+    {
+        var resultA = 0;
+        var resultB = 0;
+
+        foreach (var pos9 in _9positions)
+        {
+            var zeros = new List<Vector2>();
+            FloodFill(zeros, pos9);
+
+            resultA += zeros.ToHashSet().Count;
+            resultB += zeros.Count;
+        }
+
+        return (resultA, resultB);
+    }
+
+    void FloodFill(List<Vector2> zeros, Vector2 position)
+    {
+        if (_map[position.Y, position.X] == 0)
+        {
+            zeros.Add(position);
+            return;
+        }
+
+        foreach (var dir in _directions)
+        {
+            var newP = ValidPosition(position, dir);
+            if (newP is not null)
+            {
+                FloodFill(zeros, (Vector2)newP);
+            }
+        }
+    }
+
+    Vector2? ValidPosition(Vector2 pos, Direction2 dir)
+    {
+        var newPos = pos.Move(dir);
+        if (newPos.Y < 0 || newPos.X < 0 || newPos.Y > _map.GetLength(0) - 1 || newPos.X > _map.GetLength(1) - 1)
+            return null;
+
+        if (_map[pos.Y, pos.X] - 1 != _map[newPos.Y, newPos.X])
+            return null;
+
+        return newPos;
+    }
+}
