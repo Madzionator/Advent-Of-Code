a3497e68fa140dc7a1f4a6d3f0c617052790fee5
Magdalena Majta <majta.magdalena@gmail.com>
2025-12-04 20:19:36 +0100

    Day 4 ⭐⭐🍴🛻

---
+++ AdventOfCode2025/Days/Day4.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode2025.Helpers;
+
+namespace AdventOfCode2025.Days;
+
+[AocData("example4_1.txt", 13, 43)]
+[AocData("input4.txt", 1349, 8277)]
+public class Day4 : Day
+{
+    public override (object? PartA, object? PartB) Execute(string[] inputLines)
+    {
+        var input = new HashSet<Vector2>();
+
+        for (var y = 0; y < inputLines.Length; y++)
+        for (var x = 0; x < inputLines[y].Length; x++)
+            if (inputLines[y][x] == '@')
+            {
+                input.Add((y, x));
+            }
+
+        return Task(input);
+    }
+
+    private (int, int) Task(HashSet<Vector2> input)
+    {
+        var removed = RemoveAccessibleRolls(input);
+
+        var answerPartA = removed;
+        var answerPartB = removed;
+
+        do
+        {
+            removed = RemoveAccessibleRolls(input);
+            answerPartB += removed;
+
+        } while (removed > 0);
+
+        return (answerPartA, answerPartB);
+    }
+
+    private int RemoveAccessibleRolls(HashSet<Vector2> input)
+    {
+        var rollsToRemove = new HashSet<Vector2>();
+        var directions = Direction2.SidesAndCorners;
+
+        foreach (var rollOfPaper in input.Where(rollOfPaper => directions.Count(dir => input.Contains(rollOfPaper.Move(dir))) < 4))
+        {
+            rollsToRemove.Add(rollOfPaper);
+        }
+
+        foreach (var rollOfPaper in rollsToRemove)
+        {
+            input.Remove(rollOfPaper);
+        }
+
+        return rollsToRemove.Count;
+    }
+}
